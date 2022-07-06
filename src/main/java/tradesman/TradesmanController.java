966dd314484de9d5e32fd12bbd87ffedb7fd1207
hamzayakan <68115336+hamzayakan@users.noreply.github.com>
2022-07-06 19:01:23 +0300

    4.07 first commit

--- src/main/java/tradesman/TradesmanController.java
+++ src/main/java/tradesman/TradesmanController.java
@@ -1,4 +1,15 @@
 package tradesman;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tradesman")
 public class TradesmanController {
+
+    @GetMapping
+    public String getOk(){
+        return "ok";
+    }
 }
